6ae0cf2208b4827d1b647120f3e3a1a8efd5ece1
Valuriel <christian.poetter@hotmail.de>
2021-02-12 19:52:02 +0200

    updated the UI

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -5,7 +5,9 @@ import { connect } from 'react-redux';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './ContactData/ContactData';
 
+
 class Checkout extends Component {
+
     checkoutCancelledHandler = () => {
         this.props.history.goBack();
     }
@@ -17,8 +19,10 @@ class Checkout extends Component {
     render() {
         let summary = <Redirect to="/"/>
         if (this.props.ings) {
+            const purchasedRedirect = this.props.purchased ? <Redirect to="/"/> : null;
             summary = (
                 <div>
+                    {purchasedRedirect}
                     <CheckoutSummary 
                         ingredients={this.props.ings}
                         checkoutCancelled={this.checkoutCancelledHandler}
@@ -36,6 +40,7 @@ class Checkout extends Component {
 const mapStateToProps = state => {
     return {
         ings: state.burgerBuilder.ingredients,
+        purchased: state.order.purchased
     }
 };
 
